84dd18d2065a0224faa868fedccc2b9547902953
VasilijePantic <vasilijepantic@yahoo.com>
2018-03-07 06:02:05 +0100

    test for PATCH /todos/:id added, all tests are passing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -14,7 +14,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 
@@ -145,7 +147,7 @@ describe('DELETE /todos/:id', () => {
                 }
 
                 Todo.findById(hexId).then((todo) => {
-                    expect(todo).toNotExist();
+                    expect(todo).toBeFalsy();
                     done();
                 }).catch((e) => done(e));
             })
@@ -166,5 +168,56 @@ describe('DELETE /todos/:id', () => {
             .delete('/todos/123abc')
             .expect(404)
             .end(done);
+    });
+});
+
+
+//**************************************************************** */
+
+
+// DESCRIBE BLOCK FOR - PATCH /todos/:id
+
+describe('PATCH /todos/:id', () => {
+
+    //1st test case - should update a todo- change completed to true
+    it('should update a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var text = 'This is the new text';
+        
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({// send things that are being updated
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                // expect(res.body.todo.completedAt).toBeA('number'); // HACKERMAN ERROR FOUND
+            })
+            .end(done)
+    });
+
+    //2nd test case -should clear completedAt when completed is false
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'SUP NIGGA MA BOI';
+        
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({// send things that are being updated
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeFalsy();
+                // expect(res.body.todo.completedAt).toBeA('number'); // HACKERMAN ERROR FOUND
+                // toBeA() - not working
+            })
+            .end(done)
     });
 });
