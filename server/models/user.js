78f9cf26bd44c911b59d3fbbac142c71bbb67617
VasilijePantic <vasilijepantic@yahoo.com>
2018-03-26 19:12:11 +0200

    POST /users/login route added

--- server/models/user.js
+++ server/models/user.js
@@ -89,6 +89,30 @@ UserSchema.statics.findByToken = function(token) {
 };
 
 
+
+// /users/login auth method
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    })
+};
+
+
+
+
 // bcrypt middleware for hashing passwords 
 UserSchema.pre('save', function(next) {
     var user = this;
